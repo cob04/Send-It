94a01364a9fbbbecd135f1898c5f92db53f12513
Eric Ndung'u <ericobain@gmail.com>
2018-11-07 11:07:27 +0300

    [Chore #161773242] create_app needs no input for now

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,8 @@ from  flask import Flask
 from .api.version1 import v1
 
 
-def create_app(config_name):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_name)
     app.register_blueprint(v1)
 
     return app
