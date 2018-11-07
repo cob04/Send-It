fdbdd54baf5915aab3d493ce7bb1fd986459f092
Eric Ndung'u <ericobain@gmail.com>
2018-11-07 13:02:25 +0300

    [Chore #161773242] update tests for parcel order

--- app/api/version1/tests/test_parcel_order.py
+++ app/api/version1/tests/test_parcel_order.py
@@ -4,7 +4,7 @@ import json
 from app import create_app
 
 
-class TestDataParcel(unittest.TestCase):
+class TestParcelOrderList(unittest.TestCase):
 
     def setUp(self):
         create_app().testing = True
@@ -22,6 +22,32 @@ class TestDataParcel(unittest.TestCase):
                                   data=json.dumps(self.data),
                                   content_type="application/json")
         self.assertEqual(response.status_code, 201)
+
+    def test_get(self):
+        response = self.app.get('/api/v1/parcels')
+        self.assertEqual(response.status_code, 200)
+
+
+class TestParcelOrder(unittest.TestCase):
+
+    def setUp(self):
+        create_app().testing = True
+        self.app = create_app().test_client()
+        self.data = {
+            "sender": "bob",
+            "recipient": "linda",
+            "pickup": "home",
+            "destination": "restaurant",
+            "weight": "2kg"
+        }
+
+    def test_get_by_id_when_order_exists(self):
+        response = self.app.get('/api/v1/parcels/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_by_id_when_order_does_not_exist(self):
+        response = self.app.get('/api/v1/parcels/2')
+        self.assertEqual(response.status_code, 404)
 
 
 if __name__ == "__main__":
