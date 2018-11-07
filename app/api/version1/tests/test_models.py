83c30e80ac0519e5dedae1f5a08a5e493fb04f70
Eric Ndung'u <ericobain@gmail.com>
2018-11-07 19:05:55 +0300

    [Chore #161773242] add data afresh for each test

--- app/api/version1/tests/test_models.py
+++ app/api/version1/tests/test_models.py
@@ -10,7 +10,7 @@ class ParcelOrderStoreTests(unittest.TestCase):
         self.store = ParcelOrderStore()
 
     def tearDown(self):
-        self.store.db = []
+        self.store.db.clear()
 
     def test_adding_order_to_store(self):
         payload = self.store.save('bob', 'linda', 'home', 'restaurant', '1kg')
@@ -23,7 +23,8 @@ class ParcelOrderStoreTests(unittest.TestCase):
         self.assertEqual(self.store.db, [payload])
 
     def test_fetching_orders_in_the_store(self):
-         self.assertEqual(self.store.all(),
+        self.store.save('bob', 'linda', 'home', 'restaurant', '1kg')
+        self.assertEqual(self.store.all(),
                           [{'id': 1,
                             'sender': 'bob',
                             'recipient': 'linda',
@@ -33,6 +34,7 @@ class ParcelOrderStoreTests(unittest.TestCase):
                            }])
 
     def test_fetching_order_by_id(self):
+        self.store.save('bob', 'linda', 'home', 'restaurant', '1kg')
         self.assertEqual(self.store.fetch_by_id(1),
                          {'id': 1,
                           'sender': 'bob',
@@ -44,7 +46,6 @@ class ParcelOrderStoreTests(unittest.TestCase):
 
     def test_fetching_by_id_from_an_empty_store(self):
         store = ParcelOrderStore()
-        store.db = []
         with self.assertRaises(IndexError):
             store.fetch_by_id(1)
 
