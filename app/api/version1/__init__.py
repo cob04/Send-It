e4b1aa9ee0551638d3c94ce75bb181d8ce4df8f0
Eric Ndung'u <ericobain@gmail.com>
2018-11-09 00:44:19 +0300

    [Feature #161822006] post a new parcel delivery order

--- app/api/version1/__init__.py
+++ app/api/version1/__init__.py
@@ -3,5 +3,10 @@
 from flask import Blueprint
 from flask_restful import Api
 
+from .views import ParcelOrderList
+
+
 v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 api = Api(v1)
+
+api.add_resource(ParcelOrderList, '/parcels/', strict_slashes=False)
