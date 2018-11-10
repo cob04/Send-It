ad49ae4e4a687abae70c69e514a623168f1ef5c1
Eric Ndung'u <ericobain@gmail.com>
2018-11-10 20:00:12 +0300

    [Feature #161822628] test fetching orders by user id.

---
+++ app/api/version1/tests/test_user_endpoints.py
@@ -0,0 +1,45 @@
+# test_user_endpoints.py
+
+import json
+import unittest
+
+from app import create_app
+
+from ..models import CANCELLED, NOT_DELIVERED
+from ..models import parcel_orders
+
+
+class ParcelOrderEnpointsTests(unittest.TestCase):
+
+    def setUp(self):
+        create_app().testing = True
+        self.app = create_app().test_client()
+        self.data = {
+            "user_id": 1,
+            "sender": "bob",
+            "recipient": "linda",
+            "pickup": "home",
+            "destination": "restaurant",
+            "weight": "2kg"}
+
+    def tearDown(self):
+        parcel_orders.clear()
+
+    def test_fetching_orders_by_user_id(self):
+        self.app.post('/api/v1/parcels',
+                      data=json.dumps(self.data),
+                      content_type="application/json")
+        response = self.app.get('/api/v1/users/1/parcels')
+        self.assertEqual(response.status_code, 200)
+        expected_json = {
+            "message": "success",
+            "parcel_orders": [{
+                "id": 1,
+                "user_id": 1,
+                "sender": "bob",
+                "recipient": "linda",
+                "pickup": "home",
+                "destination": "restaurant",
+                "weight": "2kg",
+                "status": NOT_DELIVERED}]}
+        self.assertEqual(expected_json, response.get_json())
