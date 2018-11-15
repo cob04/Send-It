7322a4dbc2841c4d4579ace0ccbac7d0e56981ae
Eric Ndung'u <ericobain@gmail.com>
2018-11-15 20:00:22 +0300

    [Feature #161809308] test user signup endpoint

---
+++ app/api/version2/__init__.py
@@ -0,0 +1,21 @@
+# __init__.py
+
+from flask import Blueprint
+from flask_restful import Api
+
+from .views import (ParcelOrder, ParcelOrderList, UserParcelOrderList,
+                    ParcelOrderCancellation, UserList)
+
+
+v2 = Blueprint('v2', __name__, url_prefix='/api/v2')
+api = Api(v2)
+
+api.add_resource(ParcelOrderList, '/parcels/', strict_slashes=False)
+api.add_resource(ParcelOrder, '/parcels/<int:order_id>',strict_slashes=False)
+api.add_resource(ParcelOrderCancellation, '/parcels/<int:order_id>/cancel',
+                 strict_slashes=False)
+
+api.add_resource(UserList, '/users',
+                 strict_slashes=False)
+api.add_resource(UserParcelOrderList, '/users/<int:user_id>/parcels',
+                 strict_slashes=False)
