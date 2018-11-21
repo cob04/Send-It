6938489f2a7fe2fac2e6ccecb6c7880c36daf533
Eric Ndung'u <ericobain@gmail.com>
2018-11-21 09:53:18 +0300

    [Feature #162109840] user signup enpoint using database model

--- app/api/version3/__init__.py
+++ app/api/version3/__init__.py
@@ -1,14 +1,19 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .views import order_views as views
+from .views import order_views as orders
+from .views import user_views as users
 
 
 v3 = Blueprint('v3', __name__, url_prefix='/api/v3')
 api = Api(v3)
 
-api.add_resource(views.ParcelOrderList, '/parcels/', strict_slashes=False)
-api.add_resource(views.ParcelOrder, '/parcels/<parcel_id>', strict_slashes=False)
-
-api.add_resource(views.UserParcelOrderCancel, '/parcels/<int:parcel_id>/cancel',
+api.add_resource(orders.ParcelOrderList, '/parcels/', strict_slashes=False)
+api.add_resource(orders.ParcelOrder, '/parcels/<parcel_id>',
                  strict_slashes=False)
+
+api.add_resource(orders.UserParcelOrderCancel,
+                 '/parcels/<int:parcel_id>/cancel',
+                 strict_slashes=False)
+
+api.add_resource(users.UserList, '/auth/signup', strict_slashes=False)
