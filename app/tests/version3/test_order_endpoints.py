5d22b178e3b76c130710aa966ee3cabf2a37c9dc
Eric Ndung'u <ericobain@gmail.com>
2018-11-26 13:21:05 +0300

    [Chore #162181842] more order endpoints tests

--- app/tests/version3/test_order_endpoints.py
+++ app/tests/version3/test_order_endpoints.py
@@ -1,18 +1,202 @@
-from flask import current_app
+import json
+
 from unittest import TestCase
 
 from app import create_app
-from app.db_config import destroy_tables
+from app.api.version3.models.orders import (NOT_DELIVERED, CANCELLED,
+                                            IN_TRANSIT)
+from app.api.version3.models.users import ADMIN
+from app.db_config import create_tables, destroy_tables
 
 
-class TestParcelUserOrderEndpoints(TestCase):
- 
+class TestAuthenticatedOrderEndpoints(TestCase):
+
     def setUp(self):
         self.app = create_app('testing')
-        self.test_client = self.app.test_client()
         self.app_context = self.app.app_context()
         self.app_context.push()
+        create_tables()
+        self.client = self.app.test_client()
+        self.signup_info = {"name": "bob",
+                            "email": "bob@email.com",
+                            "password": "burgers"
+                           }
+        self.admin_signup_info = {"name": "linda",
+                                  "email": "linda@email.com",
+                                  "password": "linda",
+                                  "role": ADMIN
+                                 }
+        self.login_info = {
+            "email": "bob@email.com",
+            "password": "burgers"
+        }
+        self.admin_login_info = {
+            "email": "linda@email.com",
+            "password": "linda"
+        }
+        self.parcel_data = {
+            "sender": "bob",
+            "recipient": "linda",
+            "pickup": "home",
+            "destination": "restaurant",
+            "weight": 2
+        }
+        self.order_response = {
+                "id": 1,
+                "user_id": 1,
+                "sender": "bob",
+                "recipient": "linda",
+                "pickup": "home",
+                "destination": "restaurant",
+                "present_location": "home",
+                "weight": 2.0,
+                "status": NOT_DELIVERED
+        }
+        with self.client:
+            self.client.post("/api/v3/auth/signup",
+                             data=json.dumps(self.signup_info),
+                             content_type="application/json")
+            self.client.post("/api/v3/auth/signup",
+                             data=json.dumps(self.admin_signup_info),
+                             content_type="application/json")
+            # get normal user token
+            response = self.client.post("/api/v3/auth/login",
+                             data=json.dumps(self.login_info),
+                             content_type="application/json")
+            self.login_data = response.data.decode()
+            self.auth_token = "Bearer " + json.loads(self.login_data)['access_token']
+            # get normal admin user token
+            admin_response = self.client.post("/api/v3/auth/login",
+                                              data=json.dumps(self.admin_login_info),
+                                              content_type="application/json")
+            self.admin_login_data = admin_response.data.decode()
+            self.admin_auth_token = "Bearer " + json.loads(self.admin_login_data)['access_token']
 
     def tearDown(self):
-        destroy_tables('parcels', 'users')
+        destroy_tables("users", "parcels")
         self.app_context.pop()
+
+    def test_parcels_endpoint_post_and_get_requests(self):
+        with self.client:
+            # posting a new parcel
+            response = self.client.post(
+                "/api/v3/parcels",
+                data=json.dumps(self.parcel_data),
+                headers=dict(Authorization=self.auth_token),
+                content_type="application/json")
+            response_data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(response_data["message"], "Success")
+
+            # getting all parcels
+            response2 = self.client.get(
+                "/api/v3/parcels",
+                headers=dict(Authorization=self.auth_token))
+            response2_data = json.loads(response2.data.decode())
+            self.assertEqual(response2.status_code, 200)
+            self.assertEqual(response2_data["message"], "Success")
+            self.assertEqual(response2_data["parcel_orders"], [self.order_response])
+
+            # admin getting all parcels
+            response3 = self.client.get(
+                "/api/v3/parcels",
+                headers=dict(Authorization=self.admin_auth_token))
+            response3_data = json.loads(response3.data.decode())
+            self.assertEqual(response3.status_code, 200)
+            self.assertEqual(response3_data["message"], "Success")
+            self.assertEqual(response3_data["parcel_orders"], [self.order_response])
+
+    def test_fetching_a_parcel_order(self):
+         with self.client:
+            # posting a new parcel
+            self.client.post(
+                "/api/v3/parcels",
+                data=json.dumps(self.parcel_data),
+                headers=dict(Authorization=self.auth_token),
+                content_type="application/json")
+
+            # getting all parcels
+            response = self.client.get(
+                "/api/v3/parcels/1",
+                headers=dict(Authorization=self.auth_token))
+            response_data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response_data["message"], "Success")
+            self.assertEqual(response_data["parcel_order"], self.order_response)
+
+    def test_cancelling_an_parcel_order(self):
+         with self.client:
+            # posting a new parcel
+            self.client.post(
+                "/api/v3/parcels",
+                data=json.dumps(self.parcel_data),
+                headers=dict(Authorization=self.auth_token),
+                content_type="application/json")
+
+            # getting all parcels
+            response = self.client.put(
+                "/api/v3/parcels/1/cancel",
+                headers=dict(Authorization=self.auth_token))
+            response_data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(response_data["message"], "Success")
+            self.assertEqual(response_data["parcel_order"]["status"], CANCELLED)
+
+    def test_updating_parcel_order_status(self):
+         with self.client:
+            # posting a new parcel
+            self.client.post(
+                "/api/v3/parcels",
+                data=json.dumps(self.parcel_data),
+                headers=dict(Authorization=self.auth_token),
+                content_type="application/json")
+
+            # test using normal user
+            response2 = self.client.put(
+                "/api/v3/parcels/1/status",
+                data=json.dumps(dict(status=IN_TRANSIT)),
+                headers=dict(Authorization=self.auth_token),
+                content_type="application/json")
+            response2_data = json.loads(response2.data.decode())
+            self.assertEqual(response2.status_code, 401)
+            self.assertEqual(response2_data["message"], "Sorry, you are unauthorized")
+
+    def test_updating_parcel_order_destination(self):
+         with self.client:
+            # posting a new parcel
+            self.client.post(
+                "/api/v3/parcels",
+                data=json.dumps(self.parcel_data),
+                headers=dict(Authorization=self.auth_token),
+                content_type="application/json")
+
+            # test using normal user
+            response2 = self.client.put(
+                "/api/v3/parcels/1/destination",
+                data=json.dumps(dict(destination="Nairobi")),
+                headers=dict(Authorization=self.auth_token),
+                content_type="application/json")
+            response2_data = json.loads(response2.data.decode())
+            self.assertEqual(response2.status_code, 201)
+            self.assertEqual(response2_data["message"], "Success")
+            self.assertEqual(response2_data["parcel_order"]["destination"],
+                             "Nairobi")
+
+    def test_updating_parcel_present_location(self):
+         with self.client:
+            # post a new parcel
+            self.client.post(
+                "/api/v3/parcels",
+                data=json.dumps(self.parcel_data),
+                headers=dict(Authorization=self.auth_token),
+                content_type="application/json")
+
+            # test using normal user
+            response2 = self.client.put(
+                "/api/v3/parcels/1/presentLocation",
+                data=json.dumps(dict(present_location="Kisumu")),
+                headers=dict(Authorization=self.auth_token),
+                content_type="application/json")
+            response2_data = json.loads(response2.data.decode())
+            self.assertEqual(response2.status_code, 401)
+            self.assertEqual(response2_data["message"], "Sorry, you are unauthorized")
--- app/tests/version3/test_user_endpoints.py
+++ app/tests/version3/test_user_endpoints.py
@@ -10,7 +10,7 @@ from app.api.version3.models.users import NORMAL, ADMIN
 from app.db_config import create_tables, destroy_tables
 
 
-class TestUserManager(TestCase):
+class TestUnauthenticatedUserEndpoints(TestCase):
 
     def setUp(self):
         self.app = create_app('testing')
@@ -22,11 +22,25 @@ class TestUserManager(TestCase):
                             "email": "bob@email.com",
                             "password": "burgers"
                            }
+        self.admin_signup_data = {
+            "name": "linda",
+            "email": "linda@email.com",
+            "password": "linda",
+            "role": ADMIN
+        }
+
         self.user_data = {
             "id": 1,
             "name": "bob",
             "email": "bob@email.com",
-            "role": NORMAL
+        }
+        self.login_data = {
+            "email": "bob@email.com",
+            "password": "burgers"
+        }
+        self.admin_login_data = {
+            "email": "linda@email.com",
+            "password": "linda"
         }
 
     def tearDown(self):
@@ -53,3 +67,50 @@ class TestUserManager(TestCase):
                              "Sorry, your email is already taken")
             self.assertEqual(response2_data["error"],
                              "Email not unique")
+            
+            # test signing up an admin user
+            response3 = self.client.post("/api/v3/auth/signup",
+                                        data=json.dumps(self.admin_signup_data),
+                                        content_type="application/json")
+            response3_data = json.loads(response3.data.decode())
+            self.assertEqual(response3.status_code, 201)
+            self.assertTrue(response3_data["message"], "Success")
+            self.assertTrue(response3_data["user"]["role"], ADMIN)
+
+    def test_logging_in_a_user(self):
+        with self.client:
+            intial_response = self.client.post("/api/v3/auth/signup",
+                                               data=json.dumps(self.signup_data),
+                                               content_type="application/json")
+            response = self.client.post("/api/v3/auth/login",
+                                        data=json.dumps(self.login_data),
+                                        content_type="application/json")
+            response_data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(response_data["message"], "Success")
+            self.assertTrue(response_data["user"], self.user_data)
+            self.assertIsNotNone(response_data["access_token"])
+
+            # logging in with non existing email address
+            response2 = self.client.post("/api/v3/auth/login",
+                                         data=json.dumps(dict(
+                                             email="bobby@email.com",
+                                             password="burgers")
+                                         ),
+                                         content_type="application/json")
+            response2_data = json.loads(response2.data.decode())
+            self.assertEqual(response2.status_code, 404)
+            self.assertTrue(response2_data["message"], "Sorry, we cannot find such a user")
+            self.assertTrue(response2_data["error"], "User not found")
+
+            # loggin in with wrong password
+            response3 = self.client.post("/api/v3/auth/login",
+                                         data=json.dumps(dict(
+                                            email="bob@email.com",
+                                            password="burger")
+                                         ),
+                                         content_type="application/json")
+            response3_data = json.loads(response3.data.decode())
+            self.assertEqual(response3.status_code, 400)
+            self.assertTrue(response3_data["message"], "Sorry, your password is incorrect")
+            self.assertTrue(response3_data["error"], "Incorrect password")
